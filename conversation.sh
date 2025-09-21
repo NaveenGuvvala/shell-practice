47f9dd724c2cd070315283cfe84f976d929689c9
Naveen <guvvala4835@gmail.com>
2025-09-21 13:11:28 +0530

    conversation file to git version1

--- conversation.sh
+++ conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Hi Suresh, How are you?"
-echo "Hi Ramesh, Iam good and how are you?"
-echo "Iam doing good, what are you learning,Ramesh?"
-echo "Iam learning DevOps from Join DevOps"
+echo "Ramesh" :: "Hi Suresh, How are you?"
+echo "Suresh" :: "Hi Ramesh, Iam good and how are you?"
+echo "Ramesh" :: "Iam doing good, what are you learning,Suresh?"
+echo "Suresh" :: "Ramesh,Iam learning DevOps from Join DevOps"
